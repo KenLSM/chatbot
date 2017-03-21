4fd3367ec556863ac79e0a4fa8be6bd9fa4088e1
kenlsm <kenlsm@u.nus.edu>
2017-03-21 21:30:26 +0800

    Add generic

--- index.js
+++ index.js
@@ -35,12 +35,65 @@ app.post('/webhook/', function (req, res) {
         event = req.body.entry[0].messaging[i];
         sender = event.sender.id;
         if (event.message && event.message.text) {
-            text = event.message.text;
+            text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender);
+                continue;
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
         }
     }
     res.sendStatus(200);
 });
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+};
 
 var token = "EAARSvZC93NMABAPZCoTFIe5py33PyLdtJkYTn6QZB5ITraMVHO7Qv8R3bPnULNZAcmqKuOLzxCZCrzg36JfcwbqUN65Hdg3D71h3oNgvYPpRpbW0Hs2uiHaZBQbIJD4ZAn674tm5aQis4qES8yileuIrNnvqDonTO3MLzX9lIhfzgZDZD";
 
